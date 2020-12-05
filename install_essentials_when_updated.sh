da4b44b6e355dc99358d86ee5f77cf96ed8e04eb
Satyarth Agrahari <satyarthagrahari@gmail.com>
2020-12-05 09:17:58 +0530

    Update install_essentials_when_updated.sh

--- install_essentials_when_updated.sh
+++ install_essentials_when_updated.sh
@@ -16,7 +16,23 @@ echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nf
 exec "$SHELL"
 
 # install python build essentials
-sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+sudo apt-get install --no-install-recommends make \
+  build-essential \
+  libssl-dev \
+  zlib1g-dev \
+  libbz2-dev \
+  libreadline-dev \
+  libsqlite3-dev \
+  wget \
+  curl \
+  llvm \
+  libncurses5-dev \
+  xz-utils \
+  tk-dev \
+  libxml2-dev \
+  libxmlsec1-dev \
+  libffi-dev \
+  liblzma-dev
 
 # install and set global python interpreter
 pyenv install 3.8.5
