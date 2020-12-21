d7eb07d24b47de81490f19693395e69a81ce3ec1
Satyarth Agrahari <satyarthagrahari@gmail.com>
2020-12-21 12:24:01 +0530

    Update install_essentials_when_updated.sh

--- install_essentials_when_updated.sh
+++ install_essentials_when_updated.sh
@@ -51,6 +51,7 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb # remove installer post installation
 
-# some utility cli 
+# some utility 
 sudo apt-get install tree \
-  net-tools
+  net-tools \
+  vim
